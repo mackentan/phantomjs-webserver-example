1ffaf2bc0d19e7d2bd720d3dd603251fcc00b901
macken <mackentan@users.noreply.github.com>
2015-01-04 10:54:25 +0800

    Update server.js
    修改代码

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var server = require('webserver').create(),
 	system = require('system'),
 	fs     = require('fs'),
-	port   = system.env.PORT || 8080;
+	port   = system.env.PORT || 8888;
 
 var service = server.listen(port, function(request, response) {
 
 	if(request.method == 'POST' && request.post.url){
 		var url = request.post.url;
 
-		request_page(url, function(properties, imageuri){
+		request_page(url, function(properties){
 			response.statusCode = 200;
-			response.write(JSON.stringify(properties));	
-			response.write("\n");	
-			response.write(imageuri);
+			response.setHeader('Content-Type', 'text/html; charset=utf-8');
+			response.write(properties)
 			response.close();
 		})
 
@@ -30,8 +29,8 @@ if(service) console.log("server started - http://localhost:" + server.port);
 function request_page(url, callback){
 
 	var page = new WebPage();
-	page.clipRect = { top: 0, left: 0, width: 700, height: 400 };
-	page.viewportSize = { width: 700, height: 400 };
+	page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36';
+	page.settings.resourceTimeout=10000
 
 	page.onLoadStarted = function () {
 		console.log('loading:' + url);
@@ -40,50 +39,10 @@ function request_page(url, callback){
 	page.onLoadFinished = function (status) {
 		console.log('loaded:' + url);
 
-		var properties = {};
-
-		properties.title = page.evaluate(function () {
-			return document.title
-		});
-
-		properties.links = page.evaluate(function () {
-			return Object.keys(
-					[].reduce.call(
-						document.querySelectorAll('a'), 
-						function(memo, a){
-							if(a.protocol.indexOf('http') === 0) memo[a.href] = true;
-							return memo;
-						}
-					,{})
-				)
-		});
-
-		properties.link_areas = page.evaluate(function () {
-			var sizes = [].reduce.call(document.querySelectorAll('a'), function(memo, a){
-				var bb = a.getBoundingClientRect(),
-					area = bb.width * bb.height,
-					href = a.getAttribute('href');
-			 
-				// update the map
-				if(area){
-					memo[href] = (memo[href] || 0) + area;
-				}
-			 
-				return memo;
-			},{});
-
-			return Object.keys(sizes).map(function(url){
-				return [url, sizes[url]];
-			});
-		})
-
 		setTimeout(function(){
-			var imageuri = 'data:image/png;base64,' + page.renderBase64('png');
-
-			callback(properties,imageuri);
-
+			callback(page.content);
 			page.close();
-		},100)
+		},100)		
 	};
 
 	page.open(url);
